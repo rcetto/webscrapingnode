3da31bfa6dc67701c93c29fb96c64280838846eb
RubenDM <rcetto@gmail.com>
2020-07-17 13:07:39 +0200

    sustituimos request por axios

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const cheerio = require('cheerio');
 const fs = require('fs');
 const request = require('request');
+
+request('https://www.aemps.gob.es/acciones-informativas/alertas/medicamentos-uso-humano/', (err, res, body) => {
+    if (!err && res.statusCode == 200) {
+        
+    }
+
+});
